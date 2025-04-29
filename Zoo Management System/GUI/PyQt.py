998201087d2fe19ed3ff4ea86f1f32e2a01812d9
jeff20501 <148542805+jeff20501@users.noreply.github.com>
2025-04-29 17:15:02 +0300

    Delete Zoo Management System/GUI directory

--- Zoo Management System/GUI/PyQt.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QWidget
-
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle("My First PyQt App")
-window.resize(400, 300)
-window.show()
-sys.exit(app.exec())
